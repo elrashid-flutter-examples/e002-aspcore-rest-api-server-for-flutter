e48aecfd8c08c2b58ebadcb42ab9b8b857e3c2f3
moh <osman.elrashid@gmail.com>
2019-07-28 20:51:17 +0400

    initial files

--- Startup.cs
+++ Startup.cs
@@ -20,40 +20,6 @@ namespace App2
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
-            ////////////////////////////////
-            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
-            services.AddAuthentication(options =>
-            {
-                // Notice the schema name is case sensitive [ cookies != Cookies ]
-                options.DefaultScheme = "cookies";
-                options.DefaultChallengeScheme = "oidc";
-            })
-            //https://github.com/leastprivilege/AspNetCoreSecuritySamples/blob/aspnetcore21/OidcAndApi/src/AspNetCoreSecurity/Startup.cs
-            .AddCookie("cookies", options => options.ForwardDefaultSelector = ctx => ctx.Request.Path.StartsWithSegments("/api") ? "jwt" : "cookies")
-            .AddJwtBearer("jwt", options =>
-            {
-                options.Authority = "http://localhost:5010";
-                options.Audience = "app2api";
-                options.RequireHttpsMetadata = false;
-            })
-            .AddOpenIdConnect("oidc", options =>
-            {
-                options.SignInScheme = "cookies";
-                options.Authority = "http://localhost:5010";
-                options.RequireHttpsMetadata = false;
-                options.ClientId = "mvc";
-                options.SaveTokens = true;
-
-                options.ClientSecret = "secret";
-                options.ResponseType = "code id_token";
-                options.GetClaimsFromUserInfoEndpoint = true;
-                options.Scope.Add("app2api");
-                options.Scope.Add("offline_access");
-                //https://github.com/leastprivilege/AspNetCoreSecuritySamples/blob/aspnetcore21/OidcAndApi/src/AspNetCoreSecurity/Startup.cs
-                options.ForwardDefaultSelector = ctx => ctx.Request.Path.StartsWithSegments("/api") ? "jwt" : "oidc";
-            });
-            /////////////////////////////////
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSwaggerGen(c =>
             {
@@ -72,9 +38,7 @@ namespace App2
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
             }
-            /////////////////////////////////
-            app.UseAuthentication();
-            /////////////////////////////////
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
